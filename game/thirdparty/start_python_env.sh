cfa60af6966b3425d71d3ed53f4bf72e556e744a
Peru S <perumaal@rlplays.com>
2026-06-19 21:45:19 -0700

    c

--- game/thirdparty/start_python_env.sh
+++ game/thirdparty/start_python_env.sh
@@ -1,5 +1,5 @@
 # Run using source start_python_env.sh
-pip install uv
+command -v uv >/dev/null 2>&1 || pip install --break-system-packages uv
 [ -d "puffer" ] || uv venv puffer
 source puffer/bin/activate
 cd PufferLib
