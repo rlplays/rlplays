8342b32309cd478552459e18bcfdccb633550ea6
Peru S <perumaal@rlplays.com>
2026-06-19 21:23:48 -0700

    Remove cache

--- docker/rebuild_docker.sh
+++ docker/rebuild_docker.sh
@@ -1,6 +1,6 @@
 docker rm -f rlplays_rdp
 docker rmi rlplays:linux
-docker build -t rlplays:linux .
+docker build --no-cache -t rlplays:linux .
 docker run -d -p 3399:3389 -p 2222:22 --gpus all --name rlplays_rdp rlplays:linux
 
 echo "Built and running docker:"
