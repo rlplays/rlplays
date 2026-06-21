d478a20f885fc35c6ba0a0ddcd804051b9bd2ea3
Peru S <perumaal@rlplays.com>
2026-06-20 22:20:46 -0700

    Fix silliness with max_steps prematurely stopping the run...

--- game/coreloop/src/rl_player.cpp
+++ game/coreloop/src/rl_player.cpp
@@ -30,6 +30,7 @@ struct TRLState
     env_ = RLPlaysEnv{
         .num_obs = modelConfig.GetInt("", "env.num_obs", 0),
         .num_actions = modelConfig.GetInt("", "env.num_actions", 0),
+        .max_steps = 1000000,
         .num_frame_skips = modelConfig.GetInt("", "env.num_frame_skips", 1),
         .render_supported = 0,
     };
